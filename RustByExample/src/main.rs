5e7ee15be4bb83ccedbe6347c0896e5c784c3da7
mjoniec <mart1n.joniec@gmail.com>
2022-01-19 21:54:54 +0100

    1.1 comments, 1.2 formatted print

--- RustByExample/src/main.rs
+++ RustByExample/src/main.rs
@@ -1,6 +1,10 @@
 mod _1_hello_world;
 use _1_hello_world::say_hello;
+use _1_hello_world::comment;
+use _1_hello_world::formatted_print;
 
 fn main() {
     say_hello();
+    comment();
+    formatted_print();
 }

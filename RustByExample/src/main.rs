9185a14a72a7cf76fcd6c440affb2c3a95120bac
mjoniec <mart1n.joniec@gmail.com>
2022-01-20 00:26:21 +0100

    1 debug

--- RustByExample/src/main.rs
+++ RustByExample/src/main.rs
@@ -2,9 +2,11 @@ mod _1_hello_world;
 use _1_hello_world::say_hello;
 use _1_hello_world::comment;
 use _1_hello_world::formatted_print;
+use _1_hello_world::debug;
 
 fn main() {
     say_hello();
     comment();
     formatted_print();
+    debug();
 }

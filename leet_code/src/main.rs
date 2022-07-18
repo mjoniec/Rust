bf4c2f260687e41fc8cf0b3ebad4f8512791fd44
mjoniec <mart1n.joniec@gmail.com>
2022-07-18 18:12:51 +0200

    leet code launcher init

---
+++ leet_code/src/main.rs
@@ -0,0 +1,6 @@
+mod s0088_merge_sorted_array;
+use s0088_merge_sorted_array::say_hello;
+
+fn main() {
+    run();
+}
---
+++ leet_code/src/s0088_merge_sorted_array.rs
@@ -0,0 +1,36 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let mut i = m - 1;
+        let mut j = n - 1;
+        let mut z = m + n - 1;
+        while z >= 0 {
+            if i >= 0 && (j < 0 || nums1[i as usize] >= nums2[j as usize]) {
+                nums1[z as usize] = nums1[i as usize];
+                i -= 1;
+            } else {
+                nums1[z as usize] = nums2[j as usize];
+                j -= 1;
+            }
+            z -= 1;
+        }
+    }
+}
+
+fn run() {
+    let mut vec1 = vec![1, 2, 3, 0, 0, 0];
+    let mut vec2 = vec![2, 5, 6];
+    Solution::merge(&mut vec1, 3, &mut vec2, 3);
+    assert_eq!(vec1, vec![1, 2, 2, 3, 5, 6]);
+
+    let mut vec1 = vec![1, 2, 3];
+    let mut vec2 = vec![];
+    Solution::merge(&mut vec1, 3, &mut vec2, 0);
+    assert_eq!(vec1, vec![1, 2, 3]);
+
+    let mut vec1 = vec![0, 0, 0];
+    let mut vec2 = vec![1, 2, 3];
+    Solution::merge(&mut vec1, 0, &mut vec2, 3);
+    assert_eq!(vec1, vec![1, 2, 3]);
+}
